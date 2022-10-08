1b8dd16789b76e0bd09d52a41553d88ced57aef4
alkatra <sagargupta1827@gmail.com>
2022-10-08 14:15:49 +1100

    Minor Change

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,45 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const MONGO = "mongodb://root:root@35.174.221.105:27017";
-mongoose.connect(MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
+// const mongoose = require("mongoose");
+// const MONGO = "mongodb://root:root@35.174.221.105:27017";
+// mongoose.connect(MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 const port = 5000;
-const User = require("./models/User");
-const LED = require("./models/LED");
+// const User = require("./models/User");
+// const LED = require("./models/LED");
 
-app.get("/api/test", (req, res) => {
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 20; j++) {
-      for (let k = 0; k < 10; k++) {
-        let genid =
-          (i < 10 ? "0" : "") +
-          i +
-          (j < 10 ? "0" : "") +
-          j +
-          (k < 10 ? "0" : "") +
-          k;
-        const newLED = new LED({
-          ledid: genid,
-          state: false,
-          dim: false,
-        });
-        newLED.save();
-      }
-    }
-  }
-  res.send("The API is working!");
-}); //works
+// app.get("/api/test", (req, res) => {
+//   for (let i = 0; i < 8; i++) {
+//     for (let j = 0; j < 20; j++) {
+//       for (let k = 0; k < 10; k++) {
+//         let genid =
+//           (i < 10 ? "0" : "") +
+//           i +
+//           (j < 10 ? "0" : "") +
+//           j +
+//           (k < 10 ? "0" : "") +
+//           k;
+//         const newLED = new LED({
+//           ledid: genid,
+//           state: false,
+//           dim: false,
+//         });
+//         newLED.save();
+//       }
+//     }
+//   }
+//   res.send("The API is working!");
+// }); //works
 
 app.get("/api/user", (req, res) => {
   console.log("Got it");
@@ -50,32 +50,32 @@ app.get("/api/user", (req, res) => {
   // });
 });
 
-app.get("/api/led", (req, res) => {
-  LED.find({}, (err, users) => {
-    return err ? res.send(err) : res.send(users);
-  });
-});
+// app.get("/api/led", (req, res) => {
+//   LED.find({}, (err, users) => {
+//     return err ? res.send(err) : res.send(users);
+//   });
+// });
 
-app.get("/api/deleteled", (req, res) => {
-  deleteled().then(() => {
-    res.send("Done");
-  });
-});
+// app.get("/api/deleteled", (req, res) => {
+//   deleteled().then(() => {
+//     res.send("Done");
+//   });
+// });
 
-async function deleteled() {
-  await LED.deleteMany({});
-}
+// async function deleteled() {
+//   await LED.deleteMany({});
+// }
 
-app.get("/api/postled", (req, res) => {
-  const newUser = new User({
-    username: "admin",
-    password: "admin",
-    access: "admin",
-  });
-  newUser.save((err) => {
-    return err ? res.send(err) : res.send("successfully added new user");
-  });
-});
+// app.get("/api/postled", (req, res) => {
+//   const newUser = new User({
+//     username: "admin",
+//     password: "admin",
+//     access: "admin",
+//   });
+//   newUser.save((err) => {
+//     return err ? res.send(err) : res.send("successfully added new user");
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
