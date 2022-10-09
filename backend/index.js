d784cceaf820078ad37293355b859541949de351
alkatra <sagargupta1827@gmail.com>
2022-10-10 09:59:43 +1100

    Update cors

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,12 @@ app.use(function (req, res, next) {
   //     req.access = element.access;
   //   }
   // });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
