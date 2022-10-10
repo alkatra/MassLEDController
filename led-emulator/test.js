b2f5208345c56874326dce55e15a8ccf8b5aadfb
alkatra <sagargupta1827@gmail.com>
2022-10-10 11:18:42 +1100

    Error handling

--- led-emulator/test.js
+++ led-emulator/test.js
@@ -7,8 +7,9 @@ const URL = "http://54.208.202.63:5000/api/";
 const fetch = require("node-fetch");
 var successes = 0;
 var fails = 0;
+const RESEND_LIMIT = 2;
 
-async function get(x) {
+async function get(x, attempt) {
   let urlx = URL + "toggle";
   let response = await fetch(urlx, {
     method: "POST",
@@ -21,26 +22,13 @@ async function get(x) {
   });
 
   if (response.status == 200) {
-    // console.log("\x1b[32m%s\x1b[0m", "Success.");
+    console.log("\x1b[32m%s\x1b[0m", "Success.");
     successes++;
   } else {
-    // console.log("\x1b[31m%s\x1b[0m", "Fail. TRYING AGAIN");
-    fails++;
-    let response2 = await fetch(urlx, {
-      method: "POST",
-      headers: {
-        //   Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ledid: x }),
-      //other options
-    });
-
-    if (response2.status == 200) {
-      // console.log("\x1b[32m%s\x1b[0m", "Success AFTER TRYING AGAIN.");
-      successes++;
+    console.log("\x1b[31m%s\x1b[0m", "Fail. TRYING AGAIN");
+    if (attempt < RESEND_LIMIT) {
+      get(x, ++attempt);
     } else {
-      // console.log("\x1b[31m%s\x1b[0m", "[CRITICAL] FAILED TWICE for = " + x);
       fails++;
     }
   }
@@ -48,15 +36,17 @@ async function get(x) {
 
 async function group(string) {
   do {
+    successes = 0;
+    fails = 0;
     console.time("Total " + string);
-    await get(string + "0303");
-    await get(string + "0304");
-    await get(string + "0305");
-    await get(string + "0306");
-    await get(string + "0307");
-    await get(string + "0308");
-    await get(string + "0309");
-    await get(string + "0401");
+    await get(string + "0303", 0);
+    await get(string + "0304", 0);
+    await get(string + "0305", 0);
+    await get(string + "0306", 0);
+    await get(string + "0307", 0);
+    await get(string + "0308", 0);
+    await get(string + "0309", 0);
+    await get(string + "0401", 0);
     console.timeEnd("Total " + string);
     let rate = (successes / (successes + fails)) * 100;
     console.log("Current Success Rate: " + rate);
@@ -64,3 +54,6 @@ async function group(string) {
 }
 
 group("01");
+group("02");
+group("03");
+group("04");
