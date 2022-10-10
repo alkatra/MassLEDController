d942d6dccaf1a1a412f571fcf71cf1dd7db452e9
alkatra <sagargupta1827@gmail.com>
2022-10-10 14:37:21 +1100

    Error handling

--- backend/index.js
+++ backend/index.js
@@ -156,7 +156,7 @@ app.post("/api/toggle", async (req, res) => {
   }
 });
 
-app.get("/api/:ledid", (req, res) => {
+app.get("/api/led/:ledid", (req, res) => {
   LED.find({ ledid: req.query.ledid }, (err, users) => {
     return err ? res.status(404).send(err) : res.status(200).send(users);
   });
